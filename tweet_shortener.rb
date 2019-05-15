edec23a20a0929901b0e65948c4be52396a760d7
Trevahr Robertson <trevahr@yahoo.com>
2019-05-15 19:40:21 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,15 @@
 def dictionary
  {
   "hello" => 'hi',
-"to, two, too" => '2', 
-"for, four" => '4',
-'be' => 'b',
-'you' => 'u',
-"at" => "@", 
-"and" => "&"
+  "to" => '2', 
+  "two" => '2',
+  "too" => '2', 
+  "for" => '4', 
+  "four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@", 
+  "and" => "&"
   }
 end
 
