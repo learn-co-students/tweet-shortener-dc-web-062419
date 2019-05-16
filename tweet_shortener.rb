55031c11536619c9202857d151c08e0b91012949
Trevahr Robertson <trevahr@yahoo.com>
2019-05-16 01:59:18 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,18 +14,37 @@ def dictionary
 end
 
 def word_substituter(string)
-  
   dictionary
-  
   a = string.split
-  
+  i = 0
   a.each do |word|
-  if  dictionary.has_key?(word)
 
-  string[word] = dictionary[word]
-  
-  end
+    if  dictionary.has_key?(word.downcase)
+
+    a[i] = dictionary[word.downcase]
+    end
+    i += 1
  end
- string
+ a.join(" ")
 end
 
+
+=begin def bulk_tweet_shortener(tweets)
+
+def word_substituter(string)
+  dictionary
+  a = string.split
+  i = 0
+  a.each do |word|
+
+    if  dictionary.has_key?(word.downcase)
+
+    a[i] = dictionary[word.downcase]
+    end
+    i += 1
+ end
+ a.join(" ")
+end
+	puts word_substituter(string)
+end
+=end
