180aca1f8cda97d7e28e1efe0a40625555d0a7d5
Harum Helmy <harumendhah@gmail.com>
2019-06-20 15:38:02 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
-def dictionary
-    dictionary_words = {
+def dictionary(input)
+    list = {
         "hello" => 'hi',
         "to" => '2',
         "two" => '2',
@@ -13,11 +13,36 @@ def dictionary
         "at" => "@",
         "and" => "&"
     }
-end
-binding.pry
-def words_substituter(tweet)
-  
-    tweet.split.each { |word| puts word.downcase }
+    if list.keys.include?(input.downcase)
+        list[input.downcase]
+    else 
+        input
+    end
 end
 
+def word_substituter(tweet)
+    tweet.split.map { |word| dictionary(word) }.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+    tweets.each { |tweet| puts word_substituter(tweet) }
+end
+
+def selective_tweet_shortener(tweet)
+    if tweet.length > 140
+        word_substituter(tweet)
+    elsif tweet.length < 130
+        tweet
+    else
+        tweet
+    end
+end
+
+def shortened_tweet_truncator(tweet)
+    if selective_tweet_shortener(tweet).length > 140
+        tweet[0..136] + "..."
+    elsif tweet.length < 140
+        tweet
+    end
+end
 
