d4dbecdc849726d7aee07aa0b7af143427b4ebf4
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-20 21:47:19 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def dictionary
+words-to-be-substituted = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter
+
+
+end
